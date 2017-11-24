f439d9e08d53e9485b1620098bb11349896a1e83
Vasyl Stokolosa <vasyl.stokolosa@synapse.com>
2017-11-24 16:08:09 +0200

    Cypress visitURL config

--- app/tests/e2e/cypress/integration/create_react_redux_app_structure_spec.js
+++ app/tests/e2e/cypress/integration/create_react_redux_app_structure_spec.js
@@ -1,8 +1,8 @@
-const config = require('./../../../../config.json');
+const config = require('./../../../../../cypress.json');
 
 describe('create-react-redux-app-structure', function() {
     it('should assert that <title> is correct', function() {
-      cy.visit(config.dev.serverHost);
+      cy.visit(config.basicConfig.dev.visitURL);
   
       cy.title().should('include', 'Create React + Redux App Structure');
     });
