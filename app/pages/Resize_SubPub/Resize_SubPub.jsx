6155c9dedefd77d65efdc406fafc20a94c1b2742
Vasyl Stokolosa <vasyl.stokolosa@synapse.com>
2017-11-29 15:22:27 +0200

    Doc details

--- app/pages/Resize_SubPub/Resize_SubPub.jsx
+++ app/pages/Resize_SubPub/Resize_SubPub.jsx
@@ -12,6 +12,13 @@ export default class Resize_SubPub extends Component {
         return <div className="app-resize-subpub">
             <h1>Resize SubPub</h1>
 
+            <p>
+                <em>For more details about Resize listener here read this
+                    <a href="https://medium.com/@shystruk/do-you-still-register-window-event-listeners-in-each-component-react-in-example-31a4b1f6f1c8"
+                       title="clearIntervals() when user has a nap" target="_blank"> article</a>
+                </em>
+            </p>
+
             <Resize_SubPub_Action />
         </div>
     }
