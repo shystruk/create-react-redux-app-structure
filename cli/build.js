6d6975d9f8f11b17aefc42f151e35cca229ed0cf
Vasyl Stokolosa <vasyl.stokolosa@synapse.com>
2017-12-04 09:46:16 +0200

    CLI

--- cli/build.js
+++ cli/build.js
@@ -3,53 +3,45 @@
 require('shelljs/global');
 const path = require('path');
 const fs = require('fs');
-const figlet = require('figlet');
 const chalk = require('chalk');
 const execSync = require('child_process').execSync;
 const spawn = require('cross-spawn');
 
 function shouldUseYarn() {
-  try {
-    execSync('yarnpkg --version', { stdio: 'ignore' });
+    try {
+        execSync('yarnpkg --version', { stdio: 'ignore' });
 
-    return true;
-  } catch (e) {
-    return false;
-  }
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 const installPackages = () => {
-  console.log(chalk.white.bold('Installing Packages'));
+    console.log(chalk.white.bold('Installing Packages'));
 
-  return new Promise((resolve, reject) => {
-    let command = shouldUseYarn() ? 'yarn' : 'npm';
-    let args = ['install'];
+    return new Promise((resolve, reject) => {
+        let command = shouldUseYarn() ? 'yarn' : 'npm';
+        let args = ['install'];
 
-    const child = spawn(command, args, { stdio: 'inherit' });
+        const child = spawn(command, args, { stdio: 'inherit' });
 
-    child.on('close', code => {
-      if (code !== 0) {
-        reject({
-          command: `${command} ${args.join(' ')}`
+        child.on('close', code => {
+            if (code !== 0) {
+                reject({
+                  command: `${command} ${args.join(' ')}`
+                });
+
+                return;
+            }
+
+            resolve();
         });
-
-        return;
-      }
-
-      resolve();
-    })
-  })
+    });
 };
 
 const build = (appName) => {
-  cp('-r', __dirname + '/../.', appName);
-
-  figlet('Create React Redux App Structure', function(err, data) {
-    if (err) {
-      return;
-    }
-
-    console.log(data);
+    cp('-r', __dirname + '/../.', appName);
 
     console.log('----------------------------------------------------------');
     console.log(chalk.white.bold('Welcome to Create React Redux App Structure'));
@@ -58,16 +50,21 @@ const build = (appName) => {
     cd(appName);
 
     installPackages()
-      .then(() => {
-        console.log(chalk.white.bold('Let\'s get started'));
-        console.log(chalk.green('Step 1: cd into the newly created ' + appName + ' directory'));
-        console.log('----------------------------------------------------------');
-      })
-      .catch(error => {
-        console.log(chalk.red('An unexpected error occurred'));
-        console.log(chalk.red(error));
-      });
-  });
+        .then(() => {
+            console.log('----------------------------------------------------------');
+            console.log(chalk.white.bold('Let\'s get started'));
+            console.log(chalk.green('Step 1: cd into the newly created ' + appName + ' directory'));
+            console.log(chalk.green('Step 2: run "npm run fast-start"'));
+            console.log(chalk.white('For more details please see docs - ' +
+              'https://github.com/shystruk/create-react-redux-app-structure/blob/master/README.md'));
+            console.log('----------------------------------------------------------');
+        })
+        .catch(error => {
+            console.log('----------------------------------------------------------');
+            console.log(chalk.red('An unexpected error occurred'));
+            console.log(chalk.red(error));
+            console.log('----------------------------------------------------------');
+        });
 };
 
 module.exports = build;
--- cli/cli.js
+++ cli/cli.js
@@ -7,12 +7,12 @@ const chalk = require('chalk');
 const build = require('./build');
 
 program
-  .version(package.version)
-  .usage('<keywords>')
-  .parse(process.argv);
+    .version(package.version)
+    .usage('<keywords>')
+    .parse(process.argv);
 
 if (program.args.length > 0) {
-  spawn(build(program.args[0]), { shell: true, stdio: 'inherit' });
+    spawn(build(program.args[0]), { shell: true, stdio: 'inherit' });
 } else if (program.args.length < 1) {
-  console.log(chalk.red('Please supply a name for your new React Redux app structure'));
+    console.log(chalk.red('Please supply a name for your new React Redux app structure'));
 }
